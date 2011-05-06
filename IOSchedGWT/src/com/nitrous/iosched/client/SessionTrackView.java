9ba6d197fb2e4aab8c179b82ea598f58622bb85b
nitrousdigital@gmail.com <nitrousdigital@gmail.com@0f8a8be7-a21e-53b9-743b-78ef3b4d88b1>
2011-05-06 17:00:59 +0000

    adding session track list

---
+++ IOSchedGWT/src/com/nitrous/iosched/client/SessionTrackView.java
@@ -0,0 +1,94 @@
+package com.nitrous.iosched.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.nitrous.iosched.client.images.Images;
+import com.nitrous.iosched.client.toolbar.ActivityToolbar;
+import com.nitrous.iosched.client.toolbar.Toolbar;
+import com.nitrous.iosched.client.toolbar.ToolbarEnabledWidget;
+
+public class SessionTrackView extends Composite implements ToolbarEnabledWidget {
+	private static final Images images = GWT.create(Images.class);
+	private ActivityToolbar toolbar = new ActivityToolbar("Session Tracks");
+	private SessionTrackController controller;
+	public SessionTrackView(int width, int height) {
+		width -= 20;
+		VerticalPanel layout = new VerticalPanel();
+		layout.setStyleName("sessionTrackList");
+		layout.setWidth(width+"px");
+		layout.setVerticalAlignment(HasVerticalAlignment.ALIGN_TOP);
+		layout.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+		initWidget(layout);
+
+		for (SessionTrack track : SessionTrack.values()) {
+			HorizontalPanel row = new HorizontalPanel();
+			row.setWidth(width+"px");
+			row.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+			row.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_LEFT);
+			Label label = new Label(track.toString());
+			label.setStyleName("sessionTrackText");
+			final SessionTrack clickedTrack = track;
+			label.addClickHandler(new ClickHandler(){
+				public void onClick(ClickEvent event) {
+					if (controller != null) {
+						controller.showSessionTrack(clickedTrack);
+					}
+				}
+			});
+			row.add(label);
+			row.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);			
+			Image img = getImage(track);
+			if (img != null) {
+				img.setStyleName("sessionTrackSwatch");
+				img.addClickHandler(new ClickHandler(){
+					public void onClick(ClickEvent event) {
+						if (controller != null) {
+							controller.showSessionTrack(clickedTrack);
+						}
+					}
+				});
+				row.add(img);
+			} else {
+				HorizontalPanel fill = new HorizontalPanel();
+				fill.setSize("40px", "40px");
+				row.add(fill);
+			}
+			layout.add(row);
+		}
+	}
+
+	private Image getImage(SessionTrack track) {
+		switch (track) {
+		case All: return null;
+		case Android: return new Image(images.trackAndroid());
+		case AppEngine: return new Image(images.trackAppEngine());
+		case Chrome: return new Image(images.trackChrome());
+		case Commerce: return new Image(images.trackCommerce());
+		case DevTools: return new Image(images.trackDevTools());
+		case Geo: return new Image(images.trackGeo());
+		case GoogleAPIs: return new Image(images.trackGoogleAPIs());
+		case GoogleApps: return new Image(images.trackGoogleApps());
+		case TechTalk: return new Image(images.trackTechTalk());
+		}
+		return null;
+	}
+	public Toolbar getToolbar() {
+		return toolbar;
+	}
+
+	public SessionTrackController getController() {
+		return controller;
+	}
+
+	public void setController(SessionTrackController controller) {
+		this.controller = controller;
+	}
+}
--- IOSchedGWT/src/com/nitrous/iosched/client/images/Images.java
+++ IOSchedGWT/src/com/nitrous/iosched/client/images/Images.java
@@ -4,36 +4,51 @@ import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
 
 public interface Images extends ClientBundle {
-	@Source("bulletin.png")
-	ImageResource bulletin();
-	
-	@Source("colors.png")
-	ImageResource colors();
-	
+	// toolbar icons
 	@Source("logo.png")
 	ImageResource logo();
-	
-	@Source("map.png")
-	ImageResource map();
-	
 	@Source("home.png")
 	ImageResource home();
-	
 	@Source("refresh.png")
 	ImageResource refresh();
-	
-	@Source("sandbox.png")
-	ImageResource sandbox();
-	
-	@Source("schedule.png")
-	ImageResource schedule();
-	
 	@Source("search.png")
 	ImageResource search();
 	
+	// color bar
+	@Source("colors.png")
+	ImageResource colors();
+
+	// activity icons
+	@Source("map.png")
+	ImageResource map();
+	@Source("sandbox.png")
+	ImageResource sandbox();
+	@Source("bulletin.png")
+	ImageResource bulletin();
+	@Source("schedule.png")
+	ImageResource schedule();
 	@Source("sessions.png")
 	ImageResource sessions();
-	
 	@Source("starred.png")
 	ImageResource starred();
+	
+	// track swatches
+	@Source("android_track.png")
+	ImageResource trackAndroid();
+	@Source("app_engine_track.png")
+	ImageResource trackAppEngine();
+	@Source("chrome_track.png")
+	ImageResource trackChrome();
+	@Source("commerce_track.png")
+	ImageResource trackCommerce();
+	@Source("dev_tools_track.png")
+	ImageResource trackDevTools();
+	@Source("geo_track.png")
+	ImageResource trackGeo();
+	@Source("google_api_track.png")
+	ImageResource trackGoogleAPIs();
+	@Source("google_apps_track.png")
+	ImageResource trackGoogleApps();
+	@Source("tech_talk_track.png")
+	ImageResource trackTechTalk();
 }
