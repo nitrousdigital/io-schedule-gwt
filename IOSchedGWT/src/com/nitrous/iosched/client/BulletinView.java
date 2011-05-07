831296158b8e5327d385ac406c6e1c92e8aedbe0
nitrousdigital@gmail.com <nitrousdigital@gmail.com@0f8a8be7-a21e-53b9-743b-78ef3b4d88b1>
2011-05-07 22:03:26 +0000

    Implemented bulletin

--- IOSchedGWT/src/com/nitrous/iosched/client/BulletinView.java
+++ IOSchedGWT/src/com/nitrous/iosched/client/BulletinView.java
@@ -1,27 +1,24 @@
 package com.nitrous.iosched.client;
 
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HasVerticalAlignment;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.nitrous.iosched.client.toolbar.ActivityToolbar;
 import com.nitrous.iosched.client.toolbar.Toolbar;
 import com.nitrous.iosched.client.toolbar.ToolbarEnabledWidget;
 
-public class BulletinView extends Composite implements ToolbarEnabledWidget {
+public class BulletinView extends Composite implements ToolbarEnabledWidget, Refreshable {
 	private ActivityToolbar toolbar = new ActivityToolbar("Bulletin");
-	public BulletinView(int width, int height) {
-		VerticalPanel layout = new VerticalPanel();
-		layout.setSize(width+"px", height+"px");
-		initWidget(layout);
-		layout.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
-		layout.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-		layout.setStyleName("iconBackground");
-		
-		Label temp = new Label("Not implemented yet... Try the map instead :)");
-		temp.setStyleName("iconText");
-		layout.add(temp);
+	private HTML iframe;
+	private static final String html = "<iframe src=\"http://www.google.com/events/io/2011/mobile_announcements.html\" width=\"100%\" height=\"100%\"></iframe>"; 
+	public BulletinView(int width, int height) {		
+		iframe = new HTML();
+		iframe.setSize(width+"px", height+"px");
+		initWidget(iframe);
+		onRefresh();
+	}
+
+	public void onRefresh() {
+		iframe.setHTML(html);
 	}
 
 	public Toolbar getToolbar() {
