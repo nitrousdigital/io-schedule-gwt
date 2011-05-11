6c57e776bc101842ad503d721b068474576ad2f0
NitrousDigital@gmail.com <NitrousDigital@gmail.com@0f8a8be7-a21e-53b9-743b-78ef3b4d88b1>
2011-05-11 01:14:58 +0000

    now playing added

--- trunk/IOSchedGWT/src/com/nitrous/iosched/client/model/Configuration.java
+++ trunk/IOSchedGWT/src/com/nitrous/iosched/client/model/Configuration.java
@@ -6,11 +6,11 @@ public class Configuration {
 	private Configuration() {
 	}
 	// LIVE FEEDS
-	public static final String SESSION_FEED_URL = "http://spreadsheets.google.com/feeds/list/tmaLiaNqIWYYtuuhmIyG0uQ/od6/public/values?alt=json";
-	public static final String SANDBOX_FEED_URL = "http://spreadsheets.google.com/feeds/list/tmaLiaNqIWYYtuuhmIyG0uQ/od4/public/values?alt=json";
+//	public static final String SESSION_FEED_URL = "http://spreadsheets.google.com/feeds/list/tmaLiaNqIWYYtuuhmIyG0uQ/od6/public/values?alt=json";
+//	public static final String SANDBOX_FEED_URL = "http://spreadsheets.google.com/feeds/list/tmaLiaNqIWYYtuuhmIyG0uQ/od4/public/values?alt=json";
 	
 	// OFF-LINE MODE
-//	public static final String SESSION_FEED_URL = GWT.getHostPageBaseURL() + "/staticfeeds/sessions.json";
-//	public static final String SANDBOX_FEED_URL = GWT.getHostPageBaseURL() + "/staticfeeds/sandbox.json";
+	public static final String SESSION_FEED_URL = GWT.getHostPageBaseURL() + "/staticfeeds/sessions.json";
+	public static final String SANDBOX_FEED_URL = GWT.getHostPageBaseURL() + "/staticfeeds/sandbox.json";
 
 }
--- trunk/IOSchedGWT/src/com/nitrous/iosched/client/view/ActivityController.java
+++ trunk/IOSchedGWT/src/com/nitrous/iosched/client/view/ActivityController.java
@@ -13,6 +13,18 @@ public interface ActivityController {
 	public void showBulletin();
 	public void showSessionTrack(SessionTrack track);
 	public void showCompanyPod(CompanyPod sandbox);
+	/**
+	 * Display the detail view for a session
+	 * @param track The track to be displayed when BACK is clicked
+	 * @param entry The session entry to show
+	 */
 	public void showSessionDetail(SessionTrack track, FeedEntry entry);
+	/**
+	 * Display the detail view for a session that is currently playing.
+	 * BACK navigation should return to the now-playing view.
+	 * @param entry The entry to display
+	 */
+	public void showCurrentSessionDetail(FeedEntry entry);
+	public void showNowPlaying();
 
 }
